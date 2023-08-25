5f2e02cc91c28e3c97a85fd6ea4dd76ddcd80a67
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-26 00:21:04 +0300

    config.mk: add DIST for Makefile, NAME, indent

--- config.mk
+++ config.mk
@@ -1,29 +1,31 @@
-# lsw version
-VERSION = 0.3
+# project metadata
+NAME      = lsw
+VERSION   = 0.3
+DIST      = lsw-${VERSION}
 
 # paths
-PREFIX = /usr/local
+PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # DragonFlyBSD, FreeBSD
-#X11INC = /usr/local/include
-#X11LIB = /usr/local/lib
+#X11INC   = /usr/local/include
+#X11LIB   = /usr/local/lib
 
 # NetBSD, OpenBSD
-#X11INC = /usr/X11R6/include
-#X11LIB = /usr/X11R6/lib
+#X11INC   = /usr/X11R6/include
+#X11LIB   = /usr/X11R6/lib
 
 # Linux
-X11INC = /usr/include
-X11LIB = /usr/lib
+X11INC    = /usr/include
+X11LIB    = /usr/lib
 
 # includes and libs
-INCS = -I${X11INC}
-LIBS = -L${X11LIB} -lX11
+INCS      = -I${X11INC}
+LIBS      = -L${X11LIB} -lX11
 
 # flags
-CFLAGS   = -ansi -pedantic -Wall -DVERSION=\"${VERSION}\" ${INCS}
-LDFLAGS  = ${LIBS}
+CFLAGS    = -ansi -pedantic -Wall -DVERSION=\"${VERSION}\" ${INCS}
+LDFLAGS   = ${LIBS}
 
 # compiler and linker
-CC = cc
+CC        = cc
